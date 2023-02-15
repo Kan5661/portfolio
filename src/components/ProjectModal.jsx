53d8f879e34a16d74fa60c7aa32693a3bf28b9b2
Kan5661 <linkan0718@gmail.com>
2023-02-15 17:57:11 -0500

    close modal icon

--- src/components/ProjectModal.jsx
+++ src/components/ProjectModal.jsx
@@ -1,7 +1,11 @@
+import '../styles/ProjectModal.css'
+
 function ProjectModal({setShowProjectModal, showProjectModal}) {
     return(
         <div className="ModalContainer" id={showProjectModal? 'showModal' : 'closeModal'}>
-            <div onClick={() => setShowProjectModal(false)}>Close</div>
+            {/* <i class="fa-solid fa-xmark" onClick={() => setShowProjectModal(false)}></i> */}
+            {/* <i class="fa-regular fa-rectangle-xmark" onClick={() => setShowProjectModal(false)}></i> */}
+            <i id='closeButton' class="fa-regular fa-circle-xmark" onClick={() => setShowProjectModal(false)}></i>
         </div>
     )
 }
