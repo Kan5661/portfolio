83c99f0a2690b1d9918b3fc4892d5cc4ba0c93ec
Kan5661 <linkan0718@gmail.com>
2023-02-18 17:57:24 -0500

    scroll to anchored links

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,12 +1,17 @@
 import '../styles/Nav.css'
 import { Link } from 'react-router-dom'
+import { useEffect, useState } from 'react'
  
 function Nav() {
+    const [currentPath, setCurrentPath] = useState('')
+    const ProjectAnchor = () => {
+        if (window.location.pathname == '/') document.getElementById('projects').scrollIntoView()
+    }
     return(
         <div className="Nav" id='Nav'>
-            <a href='/#App' className='Home'>Home</a>
+            <Link to='/' onClick={() => window.scrollTo({ top: 0, behavior: 'smooth' })} className='Home'>Home</Link>
             <div className='empty'></div>
-            <a href='/#projects' className='Work'>Work</a>
+            <Link to='/' className='Work' onClick={ProjectAnchor}>Projects</Link>
             <Link to='About' className='About'>About</Link>
             <Link to='/Contact' className='Contact'>Contact</Link>
         </div>
