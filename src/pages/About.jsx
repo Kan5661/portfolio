0943d8ae6c5999c6dc143d99ad90e3930a10b617
Kan5661 <linkan0718@gmail.com>
2023-02-22 13:24:13 -0500

    button working + more stylings

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,4 +1,6 @@
 import '../styles/About.css'
+import resume from '../downloads/resume.pdf'
+const google_doc_link = 'https://docs.google.com/document/d/e/2PACX-1vSZ0PHzRPjmhqAiXHQAfaXKNrKYkXEhIELG_71hx7FUVK_ThcJ8ffngnMc2i06553VAhqZORJ1Rxni5/pub'
 
 function About() {
     return(
@@ -12,8 +14,8 @@ function About() {
                 I have just recently graduated from <a className="Span" href='https://generalassemb.ly/' target='_blank'>General Assembly</a>, a 3 month intensive coding boot camp and learned how to create Full Stack applications from scratch to production.
                </div>
                <div className="ResumeButtons">
-                    <a className='ResumeButton'>Download</a>
-                    <a className='ResumeButton'>View</a>
+                    <a href={resume}  download={'Kan_Lin_Resume.pdf'} className='ResumeButton'>Download</a>
+                    <a href={google_doc_link} className='ResumeButton'>View</a>
                </div>
             </div>
         </div>
