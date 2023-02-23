815b303ea5dbab8b7f95fbfbb4cce6a40dd80620
Kan5661 <linkan0718@gmail.com>
2023-02-22 20:33:34 -0500

    name title effect changed

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,6 +1,5 @@
 import '../styles/About.css'
 import resume from '../downloads/resume.pdf'
-const google_doc_link = 'https://docs.google.com/document/d/e/2PACX-1vSZ0PHzRPjmhqAiXHQAfaXKNrKYkXEhIELG_71hx7FUVK_ThcJ8ffngnMc2i06553VAhqZORJ1Rxni5/pub'
 
 function About() {
     return(
@@ -13,9 +12,10 @@ function About() {
                 <br></br>
                 I have just recently graduated from <a className="Span" href='https://generalassemb.ly/' target='_blank'>General Assembly</a>, a 3 month intensive coding boot camp and learned how to create Full Stack applications from scratch to production.
                </div>
+               <h2>Resume</h2>
                <div className="ResumeButtons">
                     <a href={resume}  download={'Kan_Lin_Resume.pdf'} className='ResumeButton'>Download</a>
-                    <a href={google_doc_link} className='ResumeButton'>View</a>
+                    <a href={resume} target='_blank' className='ResumeButton'>View</a>
                </div>
             </div>
         </div>
