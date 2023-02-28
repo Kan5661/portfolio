5b595f3e22f88d5fbf305041b31caa9020692bc0
Kan5661 <linkan0718@gmail.com>
2023-02-27 21:31:31 -0500

    scroll to top when click on same page of nav

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -5,18 +5,17 @@ import { useState } from 'react'
 function Nav() {
     const Navigate = useNavigate()
     const [currentPath, setCurrentPath] = useState('')
-    const ProjectAnchor = async () => {
-        // if (window.location.pathname == '/') document.getElementById('projects').scrollIntoView()
-        await Navigate('/')
+    async function ProjectAnchor(path) {
+        await Navigate(path)
         document.getElementById('projects').scrollIntoView()
     }
     return(
         <div className="Nav" id='Nav'>
             <Link to='/' onClick={() => window.scrollTo({ top: 0, behavior: 'smooth' })} className='Home'>Home</Link>
             <div className='empty'></div>
-            <div className='Work' onClick={ProjectAnchor}>Projects</div>
-            <Link to='About' className='About'>About</Link>
-            <Link to='/Contact' className='Contact'>Contact</Link>
+            <div className='Work' onClick={() => ProjectAnchor('/')}>Projects</div>
+            <Link to='About' onClick={window.scrollTo(0, 0)} className='About'>About</Link>
+            <Link to='/Contact' onClick={window.scrollTo(0, 0)} className='Contact'>Contact</Link>
         </div>
     )
 }
